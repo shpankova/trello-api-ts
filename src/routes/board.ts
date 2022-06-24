b3890691cb8a26d209d1c57cf180d60b14557e10
spankova <dshpankova282002@gmail.com>
2022-06-24 10:17:25 +0300

    added routes for boards

---
+++ src/routes/board.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router = Router();
+import controller from "../controllers/boardController";
+import roleMiddleware from '../middlewares/roles-middleware'
+
+router.post('/boards', controller.createBoard);
+router.get('/boards/:id', controller.findBoardById);
+router.put('/boards/:id', roleMiddleware(["admin"]), controller.updateBoardById)
+router.delete('/boards/:id', roleMiddleware(["admin"]), controller.deleteBoardById);
+
+export default router;
