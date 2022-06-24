a7e1d7088aa31c5228383a386f6a8dc24a42418d
spankova <dshpankova282002@gmail.com>
2022-06-24 14:07:26 +0300

    added migration for db

---
+++ migrations/20220621090221-create-card-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('card', {
+    columns: {
+      card_id: { type: 'int', primaryKey: true, autoIncrement: true },
+      board_id: { type: 'int',  name: 'board_id_card_id_fk',
+      table: 'board',
+      rules: {
+        onDelete: 'CASCADE'
+      }, },
+      name: { type: 'text', notNull: true, },
+      description: { type: 'text', notNull: true, },
+      created_at: { type: 'datetime', defaultValue: new String('CURRENT_TIMESTAMP'), notNull: true },
+      estimate: { type: 'text', notNull: true, },
+      status: { type: 'text', notNull: true, },
+      due_date: { type: 'datetime', notNull: true },
+      labels: { type: 'text', notNull: true, },
+    },
+    ifNotExists: true
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('card');
+};
+
+exports._meta = {
+  "version": 1
+};
---
+++ migrations/20220621101346-create-board-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('board', {
+    columns: {
+      board_id: { type: 'int', primaryKey: true, autoIncrement: true },
+      name: { type: 'text', notNull: true, },
+      color: { type: 'text', notNull: true, },
+      description: { type: 'text', notNull: true, },
+      created_at: { type: 'datetime', defaultValue: new String('CURRENT_TIMESTAMP'), notNull: true },
+    },
+    ifNotExists: true
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('board');
+};
+
+exports._meta = {
+  "version": 1
+};
