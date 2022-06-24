1a9f2cffcc0f8bed30bc9310736e938008fa449f
spankova <dshpankova282002@gmail.com>
2022-06-24 13:19:59 +0300

    separated the app fron the server

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,9 @@
-import express, { Application} from 'express';
 import config from "config";
-// const helmet = require('helmet')
-
-import routerBoard from './routes/board'
-import routerCard from './routes/card'
-
-// import errorMiddleware from './middlewares/error-middleware'
+import app from "./app";
 
 const PORT = config.get("port") as number;
 const HOST = config.get("host") as string;
 
-
-const app: Application = express();
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api', routerBoard)
-app.use('/api', routerCard)
-
-
-// app.use(helmet())
-// app.use(errorMiddleware)
-
-
 app.listen(PORT, HOST, () => {
     console.log(`Server listens http://${HOST}:${PORT}`);
 });
-
