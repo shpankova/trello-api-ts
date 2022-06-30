6012a274a22aa079a89eac6eafd0cfa45ec05957
spankova <dshpankova282002@gmail.com>
2022-06-30 23:42:36 +0300

    added db connection based on node env

--- db.ts
+++ db.ts
@@ -1,15 +1,29 @@
-import {Pool} from 'pg'
+import { Pool } from "pg";
 
-export const pool = new Pool({
+let config = null;
+const env = process.env.NODE_ENV;
+if (env === "development" || env === "production") {
+  config = {
     user: "postgres",
     host: "postgres",
     database: "trello-api-docker",
     password: "61665786",
-    port: 5432,
+    port: 5432
+  };
+}
+
+if (env === "test") {
+  config = {
+    user: "postgres",
+    host: "localhost",
+    database: "test",
+    password: "61665786",
+    port: 5432
+  };
+}
+
+export const pool = new Pool(config);
+
+pool.on("connect", () => {
+  console.log("DB connected succesfuly!");
 });
-
-pool.on('connect', () => {
-    console.log('DB connected succesfuly!');
-});
-
-
