56b631854d9f0cc7fc79e61bc23478139245d2cc
spankova <dshpankova282002@gmail.com>
2022-06-30 23:33:50 +0300

    added card and board value types

---
+++ types/boardType.ts
@@ -0,0 +1,16 @@
+
+export type Board = {
+    name: string,
+    color: string;
+    description: string;
+    board_id: number;
+    id: string;
+}
+
+export type ResBoard = {
+    name: string,
+    color: string;
+    description: string;
+    created_at: string;
+    board_id: number;
+}
---
+++ types/cardType.ts
@@ -0,0 +1,22 @@
+export type Card = {
+    name: string;
+    description: string;
+    estimate: string;
+    status: string;
+    due_date: string;
+    labels: string;
+    card_id: number;
+    board_id: number;
+    id: string;
+}
+
+export type ResCard = {
+    name: string;
+    description: string;
+    estimate: string;
+    status: string;
+    due_date: string;
+    labels: string;
+    card_id: number;
+    board_id: number;
+}
