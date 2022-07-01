69c82fee87c9059d2dc456f4a21a63aad228d308
spankova <dshpankova282002@gmail.com>
2022-07-01 09:33:27 +0300

    deleted sql

--- sql/createDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE "trello-api"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'German_Germany.1252'
-    LC_CTYPE = 'German_Germany.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1; 
